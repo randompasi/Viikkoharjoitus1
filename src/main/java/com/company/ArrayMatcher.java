8422690879afa7284ec070049e2fe9ace9e852dd
randomapsi <anmivu@utu.fi>
2020-10-22 12:21:16 +0300

    fixing array text

--- src/main/java/com/company/ArrayMatcher.java
+++ src/main/java/com/company/ArrayMatcher.java
@@ -5,9 +5,9 @@ public class ArrayMatcher {
     /**
      *  check if 2d shape in A array will fit in new array B
      *  Assuming that 2d arays are square
-     * @.pre  A.length>0 && A.[A.length]>0 && B.length>0 && B.[B.length]>0 && A != null && B != null
-     *x and y are uppder left corner coordination of shape coordination in Array A
-     * @.post RESULT == FORALL(x,y ; x >= 0 && y >= 0; x < B.length && y < B.[B.length])
+     * @.pre  A != null && B != null &&  A.length>0 && A.[A.length]>0 && B.length>0 && B.[B.length]>0
+     *
+     * @.post RESULT ==FORALL(int x,int y; A[x][y]<*; EXIST(z,c ; z >= 0 && c >= 0; z < B.length && c < B.[B.length]))
      *
      */
 
